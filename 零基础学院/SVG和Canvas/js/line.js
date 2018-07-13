afcbf12c26f7d01e3d249d4b3795376b959936e8
renhaowansui <31428222+renhaowansui@users.noreply.github.com>
2018-07-13 17:37:45 +0800

    7.13

---
+++ 零基础学院/SVG和Canvas/js/line.js
@@ -0,0 +1,27 @@
+function draw(){
+	var x = 500;
+	var y = 400;
+	var space = 40;
+	var canvas = document.getElementById("canvas");
+	if(canvas.getContext){
+		var g = canvas.getContext("2d");
+		g.lineWidth = 5;
+		g.moveTo(x,y);
+		g.lineTo(0,y);
+		g.lineTo(0,0);
+		g.stroke();
+		g.lineWidth = 2;
+		for(var i = 0; i<sourceData[0].sale.length; i++){
+			g.beginPath();
+			g.arc((i+1)*space, sourceData[0].sale[i], 2, 0, Math.PI*2);
+			g.fillStyle = "black";
+			g.fill();
+			if( i != 0){
+				g.moveTo((i)*space, sourceData[0].sale[i-1]);
+				g.lineTo((i+1)*space, sourceData[0].sale[i]);
+			}
+			g.stroke();
+		}
+	}
+}
+draw();
